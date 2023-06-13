9cbde0fee2d240d93f4a941208529d5dc34394c5
junho <80895090+wjswnsgh98@users.noreply.github.com>
2023-06-13 11:36:27 +0900

    [unrated] Title: 0 떼기, Time: 10.58 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int i = Integer.parseInt(n_str);
+        answer += i +"";
+        return answer;
+    }
+}
