14eb74b210932e1272884bd8f898ee872dc401d5
junho <80895090+wjswnsgh98@users.noreply.github.com>
2023-06-16 18:07:42 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String result = Integer.toString(order);
+        for(int i = 0; i < result.length(); i++){
+            if(result.charAt(i) == '3' || result.charAt(i) == '6' || result.charAt(i) == '9'){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
