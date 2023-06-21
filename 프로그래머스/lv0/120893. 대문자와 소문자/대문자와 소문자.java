7dfb5230584c906108efccae46e3afba8a73d819
junho <80895090+wjswnsgh98@users.noreply.github.com>
2023-06-21 19:39:56 +0900

    [level 0] Title: 대문자와 소문자, Time: 14.07 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i = 0; i < my_string.length(); i++){
+            if(65 <= my_string.charAt(i) && my_string.charAt(i) <= 90){
+                answer += (char) (my_string.charAt(i) + 32);
+            } else{
+                answer += (char) (my_string.charAt(i) - 32);
+            }
+        }
+        return answer;
+    }
+}
