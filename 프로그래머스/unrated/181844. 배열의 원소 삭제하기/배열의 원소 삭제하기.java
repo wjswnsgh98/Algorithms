054b3fbb590c5e4e2561eab3f609671213452165
junho <80895090+wjswnsgh98@users.noreply.github.com>
2023-07-06 17:25:21 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 11.88 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -1,24 +1,46 @@
+// class Solution {
+//     public int[] solution(int[] arr, int[] delete_list) {
+//         int size = arr.length;
+//         boolean[] isDeleted = new boolean[size];
+//         int resultSize = size;
+//         for(int num : delete_list){
+//             for(int i = 0 ; i < size ; i++){
+//                 if(arr[i] == num){
+//                     isDeleted[i] = true;
+//                     resultSize--;
+//                     break;
+                    
+//                 }
+//             }
+//         }
+//         int[] answer = new int[resultSize];
+//         int index = 0;
+//         for(int i = 0 ; i < size ; i++){
+//             if(!isDeleted[i]){
+//                 answer[index++] = arr[i];
+//             }
+//         }
+//         return answer;
+//     }
+// }
+import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
-        int size = arr.length;
-        boolean[] isDeleted = new boolean[size];
-        int resultSize = size;
-        for(int num : delete_list){
-            for(int i = 0 ; i < size ; i++){
-                if(arr[i] == num){
-                    isDeleted[i] = true;
-                    resultSize--;
+        ArrayList<Integer> list = (ArrayList<Integer>) Arrays
+            .stream(arr).boxed().collect(Collectors.toList());
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < delete_list.length; j++) {
+                if (delete_list[j] == arr[i]) {
+                    list.remove((Integer) arr[i]);
                     break;
-                    
                 }
-            }
+            }    
         }
-        int[] answer = new int[resultSize];
-        int index = 0;
-        for(int i = 0 ; i < size ; i++){
-            if(!isDeleted[i]){
-                answer[index++] = arr[i];
-            }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).intValue();
         }
         return answer;
     }
