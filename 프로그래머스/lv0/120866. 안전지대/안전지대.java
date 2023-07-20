e2a291907dda25ba1c71176d8dba4c126f33d33d
junho <80895090+wjswnsgh98@users.noreply.github.com>
2023-07-20 17:14:22 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.java
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.java
@@ -1,25 +1,28 @@
 class Solution {
     public int solution(int[][] board) {
-        int[][] bomb = new int[board.length + 2][board.length + 2];
-        int answer = 0;
+        boolean[][] bomb = new boolean[board.length][board.length];
+        int[] dx = {0, 1, 1, 1, 0, -1, -1, -1};
+        int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+        
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board.length; j++){
                 if(board[i][j] == 1){
-                    for(int a = (i-1) + 1; a <= (i+1) + 1; a++){
-                        for(int b = (j-1) + 1; b <= (j+1) + 1; b++){
-                            bomb[a][b] += 1;
-                            if(bomb[a][b] == 1){
-                                continue;
-                            }
+                    bomb[i][j] = true;
+                    for(int n = 0; n < 8; n++){
+                        int x = i + dx[n];
+                        int y = j + dy[n];
+                        if(x >= 0 && x < board.length && y >= 0 && y < board.length){
+                            bomb[x][y] = true;
                         }
                     }
                 }
             }
         }
         
-        for(int i = 1; i < bomb.length - 1; i++){
-            for(int j = 1; j < bomb.length - 1; j++){
-                if(bomb[i][j] == 0){
+        int answer = 0;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                if(!bomb[i][j]){
                     answer++;
                 }
             }
