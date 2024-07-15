74e0711a96714e5c64f3c140ca71f78d2a1c436d
junho <80895090+wjswnsgh98@users.noreply.github.com>
2024-07-16 05:56:29 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.59 ms, Memory: 64 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        Arrays.sort(weights);
+        Map<Double, Integer> map = new HashMap<>();
+        
+        for(int w : weights){
+            double a = w * (1.0);
+            double b = w * (2.0) / 3;
+            double c = w * (2.0) / 4;
+            double d = w * (3.0) / 4;
+            
+            if(map.containsKey(a)){
+                answer += map.get(a);
+            }
+            if(map.containsKey(b)){
+                answer += map.get(b);
+            }
+            if(map.containsKey(c)){
+                answer += map.get(c);
+            }
+            if(map.containsKey(d)){
+                answer += map.get(d);
+            }
+            map.put((w * 1.0), map.getOrDefault((w * 1.0), 0) + 1);
+        }
+        return answer;
+    }
+}
