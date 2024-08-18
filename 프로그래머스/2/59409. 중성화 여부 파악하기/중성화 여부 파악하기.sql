80265b410cf9a8c067ecde2c530d23af19c8a3d6
junho <80895090+wjswnsgh98@users.noreply.github.com>
2024-08-18 10:52:23 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -2,7 +2,9 @@
 SELECT ANIMAL_ID
      , NAME
      , CASE 
-        WHEN SEX_UPON_INTAKE LIKE '%Intact%' THEN 'X'
+        WHEN SEX_UPON_INTAKE 
+        LIKE '%Intact%' 
+        THEN 'X'
         ELSE 'O'
        END "중성화"
 FROM ANIMAL_INS
